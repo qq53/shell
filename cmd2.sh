65bd742cdc5772e6bb4490a87dd13167ca696fa3
Sen1993 <gsen1993@gmail.com>
2014-02-07 16:47:24 +0800

    fix 3 files

--- cmd2.sh
+++ cmd2.sh
@@ -1,7 +1,7 @@
 #  File : cmd.sh
 #  ------------------------------------
 #  Create date : 2014-01-17 17:03
-#  Modified date: 2014-02-07 16:44
+#  Modified date: 2014-02-07 15:19
 #  Author : Sen1993
 #  Email : 1730806439@qq.com
 #  ------------------------------------
@@ -24,38 +24,63 @@ fi
 param=$(cat -n ./bash/"$filename".txt |\
 	egrep -i "^\s+[0-9]+\s\b\w+\b:?$" | sed 's/://g')
 param2=$(echo "$param" | awk -F ' ' '{print $2}')
+declare -Ai data
+declare -i sum
 
 echo -e 'These KEYWORD can see :\n'
-echo -e "$param2\n"
+for key in $param2; do
+	sum=$(echo "$param" | egrep -i "$key" | awk -F ' ' '{print $1}')
+	[ "$sum" -gt 0 ] && data[$key]=$(($sum)) && echo "$key "
+done
+echo
+
+declare -Ai nextarr
+declare -i j
+declare -i i=0
+tmp=$(echo ${data[@]} | sed 's/ /\n/g' | sort -n)
+for j in $tmp; do
+	nextarr[$i]=$j
+	i=$i+1
+done
 
 declare -i flag
-declare -i i
-declare -i j
-declare -i k
 while [ 1 ]; do
 	keyword=""
 	flag=0
-	k=0
-
+	
 	read -p "Input which keyword (default filename && q to exit) : " keyword
 	echo "$keyword" | grep -i "q" > /dev/null && exit 0
-	[ -z "$keyword" ] && keyword=$filename
-	for key in $param2; do
-		k=$k+1
-		if echo "$keyword" | grep -i "$key" > /dev/null; then
-			flag=1 
+	for key in ${!data[@]}; do
+		if echo "$keyword" | grep -i "$key" > /dev/null; then 
+			flag=1
 			break
 		fi
 	done
+	if [ "$flag" -eq 0 ] && [ -z "$keyword" ]; then
+		keyword=$filename
+		flag=1
+	fi
 	[ "$flag" -eq 0 ] && echo "not find this keyword !!" && continue 1
 
-	i=$(echo "$param" | sed -n "$k"'p' | awk -F ' ' '{print $1}')
-	j=$(echo "$param" | sed -n "$(($k+1))p" | awk -F ' ' '{print $1}')
-
-	if [ "$j" -gt 0 ]; then
+	flag=2
+	declare -i k
+	for key in ${!data[@]};  do
+		if echo "$key" | grep -i "$keyword" > /dev/null; then
+			i=${data[$key]}
+			for j in ${nextarr[@]}; do
+				if [ "$flag" -eq 0 ]; then
+					flag=1
+					break
+				fi
+				[ "$j" -eq "$i" ] && flag=0
+			done
+			break
+		fi
+	done
+	if [ "$flag" -eq 1 ]; then
 		j=$j-1
 		sed -n $i','$j'p' ./bash/"$filename".txt | grep ".*" --color
 	else
-		sed -n $i',$p' ./bash/"$filename".txt |grep ".*" --color
+		sed -n $i',$p' ./bash/"$filename".txt | grep ".*" --color
 	fi
 done
