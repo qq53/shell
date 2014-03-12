5a00d0d8498f8bee67cecfaa31aba84da7fbf563
Sen1993 <gsen1993@gmail.com>
2014-03-12 20:01:03 +0800

    wget

--- wget.sh
+++ wget.sh
@@ -1,7 +1,7 @@
-#  File : 1.sh
+#  File : wget.sh
 #  ------------------------------------
 #  Create date : 2014-03-11 19:50
-#  Modified date: 2014-03-11 21:01
+#  Modified date: 2014-03-11 21:41
 #  Author : Sen1993
 #  Email : 1730806439@qq.com
 #  ------------------------------------
@@ -11,15 +11,13 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 base="http://round2.sinaapp.com/"
-aa=$base
+aa=""
 declare -i i=1
 
 while true; do
-	aa=$(wget -q $aa && cat *.htm* | egrep -o "\w+.htm")
+	aa=$(curl $base$aa | egrep -o "\w+.htm")
 	aa=$(echo $aa | awk -F ' ' '{print $2}')
 	echo $aa
-	aa=$base$aa
 	i+=1
 	[ $i -ge 998 ] && break 1
-	rm *.htm*
 done
